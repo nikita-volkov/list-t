e3180458bb5a8db1f6b7b5efce86dc8f79af31d0
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-08-04 21:22:14 +0400

    MonadListTrans class

--- library/ListT.hs
+++ library/ListT.hs
@@ -20,9 +20,8 @@ instance Monad m => Monoid (ListT m a) where
             return (Just (h1, (mappend s1' (ListT m2))))
 
 instance Functor m => Functor (ListT m) where
-  fmap f s =
-    ListT $ 
-      (fmap . fmap) (\(a, b) -> (f a, fmap f b)) $ uncons s
+  fmap f (ListT m) =
+    ListT $ (fmap . fmap) (\(a, b) -> (f a, fmap f b)) m
 
 instance (Monad m, Functor m) => Applicative (ListT m) where
   pure = 
@@ -67,43 +66,53 @@ instance MFunctor ListT where
     ListT $ f $ m >>= return . fmap (\(h, t) -> (h, hoist f t))
 
 
+-- * Class
+-------------------------
+
+class MonadTrans t => MonadListTrans t where
+  uncons :: t m a -> m (Maybe (a, t m a))
+  cons :: Monad m => a -> t m a -> t m a
+
+instance MonadListTrans ListT where
+  {-# INLINE uncons #-}
+  uncons (ListT m) = 
+    m
+  cons h t = 
+    ListT $ return (Just (h, t))
+
+
 -- * Execution in the base monad
 -------------------------
 
-{-# INLINABLE uncons #-}
-uncons :: ListT m a -> m (Maybe (a, ListT m a))
-uncons (ListT m) = 
-  m
-
 {-# INLINABLE head #-}
-head :: Monad m => ListT m a -> m (Maybe a)
+head :: (Monad m, MonadListTrans t) => t m a -> m (Maybe a)
 head =
   liftM (fmap fst) . uncons
 
 {-# INLINABLE tail #-}
-tail :: Monad m => ListT m a -> m (Maybe (ListT m a))
+tail :: (Monad m, MonadListTrans t) => t m a -> m (Maybe (t m a))
 tail =
   liftM (fmap snd) . uncons
 
 {-# INLINABLE null #-}
-null :: Monad m => ListT m a -> m Bool
+null :: (Monad m, MonadListTrans t) => t m a -> m Bool
 null =
   liftM (maybe True (const False)) . uncons
 
 {-# INLINABLE fold #-}
-fold :: Monad m => (r -> a -> m r) -> r -> ListT m a -> m r
+fold :: (Monad m, MonadListTrans t) => (r -> a -> m r) -> r -> t m a -> m r
 fold s r = 
   uncons >=> maybe (return r) (\(h, t) -> s r h >>= \r' -> fold s r' t)
 
 -- |
 -- Convert to a list.
 {-# INLINABLE toList #-}
-toList :: Monad m => ListT m a -> m [a]
+toList :: (Monad m, MonadListTrans t) => t m a -> m [a]
 toList =
   liftM ($ []) . fold (\f e -> return $ f . (e :)) id
 
 {-# INLINABLE traverse_ #-}
-traverse_ :: Monad m => (a -> m ()) -> ListT m a -> m ()
+traverse_ :: (Monad m, MonadListTrans t) => (a -> m ()) -> t m a -> m ()
 traverse_ f =
   fold (const f) ()
 
@@ -113,42 +122,41 @@ traverse_ f =
 -- |
 -- Construct from any foldable.
 {-# INLINABLE fromFoldable #-}
-fromFoldable :: (Monad m, Foldable f) => f a -> ListT m a
+fromFoldable :: (Monad m, Foldable f, MonadListTrans t, MonadPlus (t m)) => f a -> t m a
 fromFoldable = 
-  foldr cons mempty
+  foldr cons mzero
 
 -- |
 -- Construct by unfolding a pure data structure.
 {-# INLINABLE unfold #-}
-unfold :: Monad m => (b -> Maybe (a, b)) -> b -> ListT m a
+unfold :: (Monad m, MonadListTrans t, MonadPlus (t m)) => (b -> Maybe (a, b)) -> b -> t m a
 unfold f s =
-  maybe mempty (\(h, t) -> cons h (unfold f t)) (f s)
+  maybe mzero (\(h, t) -> cons h (unfold f t)) (f s)
 
 -- |
 -- Produce an infinite stream.
 {-# INLINABLE repeat #-}
-repeat :: Monad m => a -> ListT m a
+repeat :: (Monad m, MonadListTrans t) => a -> t m a
 repeat = fix . cons
 
 -- * Transformation
 -------------------------
 
-{-# INLINABLE cons #-}
-cons :: Monad m => a -> ListT m a -> ListT m a
-cons h t =
-  ListT $ return (Just (h, t))
-
 {-# INLINABLE traverse #-}
-traverse :: Monad m => (a -> m b) -> ListT m a -> ListT m b
+traverse :: (Monad m, MonadListTrans t, MonadPlus (t m)) => (a -> m b) -> t m a -> t m b
 traverse f s =
-  ListT $ 
-    uncons s >>= mapM (\(h, t) -> f h >>= \h' -> return (h', traverse f t))
+  lift (uncons s) >>= 
+  mapM (\(h, t) -> lift (f h) >>= \h' -> cons h' (traverse f t)) >>=
+  maybe mzero return
 
 {-# INLINABLE take #-}
-take :: Monad m => Int -> ListT m a -> ListT m a
+take :: (Monad m, MonadListTrans t, MonadPlus (t m)) => Int -> t m a -> t m a
 take =
   \case
-    n | n > 0 ->
-      ListT . (liftM . fmap) (\(h, t) -> (h, take (pred n) t)) . uncons
+    n | n > 0 -> \t ->
+      lift (uncons t) >>= 
+        \case
+          Nothing -> t
+          Just (h, t) -> cons h (take (pred n) t)
     _ ->
-      mempty
+      const $ mzero
