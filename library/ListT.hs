nikita-volkov/list-t

commit d06b4ac61be52ac0527ae1e27b0fdd495859e6fa

Refactoring

library/ListT.hs
@@ -116,7 +116,7 @@ instance MonadIO m => MonadIO (ListT m) where
 
 instance MFunctor ListT where
   hoist f (ListT m) =
-    ListT $ f $ m >>= return . fmap (\(h, t) -> (h, hoist f t))
+    ListT $ f $ (fmap . fmap) (\(h, t) -> (h, hoist f t)) m
 
 instance MMonad ListT where
   embed f (ListT m) =

commit 58daaba3afe0ccb67dce9f7779f0336f263a0979

avoid compilation problems introduced in d06b4ac

library/ListT.hs
@@ -116,7 +116,7 @@ instance MonadIO m => MonadIO (ListT m) where
 
 instance MFunctor ListT where
   hoist f =
-    ListT . f . (fmap . fmap) (id *** hoist f) . unListT
+    ListT . f . (liftM . fmap) (id *** hoist f) . unListT
 
 instance MMonad ListT where
   embed f (ListT m) =