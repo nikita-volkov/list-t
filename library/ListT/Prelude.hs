ce79caa5649495b7e81ffc7cd07b8a4c06087fef
David Feuer <David.Feuer@gmail.com>
2021-08-26 14:34:50 -0400

    A near-clone of the LogicT API

--- library/ListT/Prelude.hs
+++ library/ListT/Prelude.hs
@@ -37,6 +37,7 @@ import Data.Foldable as Exports
 import Data.Function as Exports hiding (id, (.))
 import Data.Functor as Exports
 import Data.Functor.Classes as Exports
+import Data.Functor.Identity as Exports
 import Data.Int as Exports
 import Data.IORef as Exports
 import Data.Ix as Exports
